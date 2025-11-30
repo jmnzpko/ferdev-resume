a3a59e9d948595944df3213b699de37107e2bcff
Fernando <jmnzpko@gmail.com>
2025-11-30 02:09:05 -0700

    Fix Google Tag Manager implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,7 @@ import type {AppProps} from 'next/app';
 import {memo} from 'react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 });
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,37 @@
 import {Head, Html, Main, NextScript} from 'next/document';
 
-// next/document <Head /> vs next/head <Head />
-//
-// next/document Head is rendered once on the server. This is different from next/head which will
-// rebuild the next/head fields each time it's called, and won't overwrite next/document's Head.
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        {/* google translate breaks react:
-          - https://github.com/facebook/react/issues/11538
-          - https://bugs.chromium.org/p/chromium/issues/detail?id=872770 */}
         <meta content="notranslate" name="google" />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-JFML9F6B');
+            `,
+          }}
+        />
       </Head>
       <body className="bg-black">
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            height="0"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-JFML9F6B"
+            style={{display: 'none', visibility: 'hidden'}}
+            width="0"
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
